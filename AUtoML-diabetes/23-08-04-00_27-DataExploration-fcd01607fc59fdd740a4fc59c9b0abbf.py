ce3aaf1da281243e980aec0420774d918c721891
Sohail Hosseini <S.SADATHOSSEINI@GMAIL.COM>
2023-08-03 21:22:42 -0400

    first commit

---
+++ AUtoML-diabetes/23-08-04-00_27-DataExploration-fcd01607fc59fdd740a4fc59c9b0abbf.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Data Exploration
+# MAGIC This notebook performs exploratory data analysis on the dataset.
+# MAGIC To expand on the analysis, attach this notebook to the **Sohail Hosseini's Cluster** cluster,
+# MAGIC edit [the options of pandas-profiling](https://pandas-profiling.github.io/pandas-profiling/docs/master/rtd/pages/advanced_usage.html), and rerun it.
+# MAGIC - Explore completed trials in the [MLflow experiment](#mlflow/experiments/557053454446975/s?orderByKey=metrics.%60val_f1_score%60&orderByAsc=false)
+# MAGIC - Navigate to the parent notebook [here](#notebook/557053454446976) (If you launched the AutoML experiment using the Experiments UI, this link isn't very useful.)
+# MAGIC
+# MAGIC Runtime Version: _10.4.x-cpu-ml-scala2.12_
+
+# COMMAND ----------
+
+import os
+import uuid
+import shutil
+import pandas as pd
+import databricks.automl_runtime
+
+from mlflow.tracking import MlflowClient
+
+# Download input data from mlflow into a pandas DataFrame
+# Create temporary directory to download data
+temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], "tmp", str(uuid.uuid4())[:8])
+os.makedirs(temp_dir)
+
+# Download the artifact and read it
+client = MlflowClient()
+training_data_path = client.download_artifacts("c8daf7e8bbe743b794c46f66869f8146", "data", temp_dir)
+df = pd.read_parquet(os.path.join(training_data_path, "training_data"))
+
+# Delete the temporary data
+shutil.rmtree(temp_dir)
+
+target_col = "Outcome"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Profiling Results
+
+# COMMAND ----------
+
+from pandas_profiling import ProfileReport
+df_profile = ProfileReport(df, title="Profiling Report", progress_bar=False, infer_dtypes=False)
+profile_html = df_profile.to_html()
+
+displayHTML(profile_html)
---
+++ AUtoML-diabetes/23-08-04-00_27-LogisticRegression-61a720850f8db7342c6f432813d328f7.py
@@ -0,0 +1,322 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Logistic Regression training
+# MAGIC This is an auto-generated notebook. To reproduce these results, attach this notebook to the **Sohail Hosseini's Cluster** cluster and rerun it.
+# MAGIC - Compare trials in the [MLflow experiment](#mlflow/experiments/557053454446975/s?orderByKey=metrics.%60val_f1_score%60&orderByAsc=false)
+# MAGIC - Navigate to the parent notebook [here](#notebook/557053454446976) (If you launched the AutoML experiment using the Experiments UI, this link isn't very useful.)
+# MAGIC - Clone this notebook into your project folder by selecting **File > Clone** in the notebook toolbar.
+# MAGIC
+# MAGIC Runtime Version: _10.4.x-cpu-ml-scala2.12_
+
+# COMMAND ----------
+
+import mlflow
+import databricks.automl_runtime
+
+target_col = "Outcome"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load Data
+
+# COMMAND ----------
+
+from mlflow.tracking import MlflowClient
+import os
+import uuid
+import shutil
+import pandas as pd
+
+# Create temp directory to download input data from MLflow
+input_temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], "tmp", str(uuid.uuid4())[:8])
+os.makedirs(input_temp_dir)
+
+
+# Download the artifact and read it into a pandas DataFrame
+input_client = MlflowClient()
+input_data_path = input_client.download_artifacts("c8daf7e8bbe743b794c46f66869f8146", "data", input_temp_dir)
+
+df_loaded = pd.read_parquet(os.path.join(input_data_path, "training_data"))
+# Delete the temp data
+shutil.rmtree(input_temp_dir)
+
+# Preview data
+df_loaded.head(5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Select supported columns
+# MAGIC Select only the columns that are supported. This allows us to train a model that can predict on a dataset that has extra columns that are not used in training.
+# MAGIC `[]` are dropped in the pipelines. See the Alerts tab of the AutoML Experiment page for details on why these columns are dropped.
+
+# COMMAND ----------
+
+from databricks.automl_runtime.sklearn.column_selector import ColumnSelector
+supported_cols = ["Age", "BloodPressure", "Insulin", "BMI", "SkinThickness", "DiabetesPedigreeFunction", "Pregnancies", "Glucose"]
+col_selector = ColumnSelector(supported_cols)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Preprocessors
+
+# COMMAND ----------
+
+transformers = []
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Numerical columns
+# MAGIC
+# MAGIC Missing values for numerical columns are imputed with mean by default.
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+
+num_imputers = []
+num_imputers.append(("impute_mean", SimpleImputer(), ["Age", "BMI", "BloodPressure", "DiabetesPedigreeFunction", "Glucose", "Insulin", "Pregnancies", "SkinThickness"]))
+
+numerical_pipeline = Pipeline(steps=[
+    ("converter", FunctionTransformer(lambda df: df.apply(pd.to_numeric, errors="coerce"))),
+    ("imputers", ColumnTransformer(num_imputers, sparse_threshold=0))
+])
+
+transformers.append(("numerical", numerical_pipeline, ["Age", "BloodPressure", "Insulin", "BMI", "SkinThickness", "DiabetesPedigreeFunction", "Pregnancies", "Glucose"]))
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+
+preprocessor = ColumnTransformer(transformers, remainder="passthrough", sparse_threshold=0)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Feature standardization
+# MAGIC Scale all feature columns to be centered around zero with unit variance.
+
+# COMMAND ----------
+
+from sklearn.preprocessing import StandardScaler
+
+standardizer = StandardScaler()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train - Validation - Test Split
+# MAGIC Split the input data into 3 sets:
+# MAGIC - Train (60% of the dataset used to train the model)
+# MAGIC - Validation (20% of the dataset used to tune the hyperparameters of the model)
+# MAGIC - Test (20% of the dataset used to report the true performance of the model on an unseen dataset)
+
+# COMMAND ----------
+
+from sklearn.model_selection import train_test_split
+
+split_X = df_loaded.drop([target_col], axis=1)
+split_y = df_loaded[target_col]
+
+# Split out train data
+X_train, split_X_rem, y_train, split_y_rem = train_test_split(split_X, split_y, train_size=0.6, random_state=533288322, stratify=split_y)
+
+# Split remaining data equally for validation and test
+X_val, X_test, y_val, y_test = train_test_split(split_X_rem, split_y_rem, test_size=0.5, random_state=533288322, stratify=split_y_rem)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train classification model
+# MAGIC - Log relevant metrics to MLflow to track runs
+# MAGIC - All the runs are logged under [this MLflow experiment](#mlflow/experiments/557053454446975/s?orderByKey=metrics.%60val_f1_score%60&orderByAsc=false)
+# MAGIC - Change the model parameters and re-run the training cell to log a different trial to the MLflow experiment
+# MAGIC - To view the full list of tunable hyperparameters, check the output of the cell below
+
+# COMMAND ----------
+
+from sklearn.linear_model import LogisticRegression
+
+help(LogisticRegression)
+
+# COMMAND ----------
+
+import mlflow
+import sklearn
+from sklearn import set_config
+from sklearn.pipeline import Pipeline
+
+set_config(display="diagram")
+
+sklr_classifier = LogisticRegression(
+  C=0.08488618549946696,
+  penalty="l2",
+  random_state=533288322,
+)
+
+model = Pipeline([
+    ("column_selector", col_selector),
+    ("preprocessor", preprocessor),
+    ("standardizer", standardizer),
+    ("classifier", sklr_classifier),
+])
+
+model
+
+# COMMAND ----------
+
+# Enable automatic logging of input samples, metrics, parameters, and models
+mlflow.sklearn.autolog(log_input_examples=True, silent=True)
+
+with mlflow.start_run(experiment_id="557053454446975", run_name="logistic_regression") as mlflow_run:
+    model.fit(X_train, y_train)
+    
+    # Training metrics are logged by MLflow autologging
+    # Log metrics for the validation set
+    sklr_val_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_val, y_val, prefix="val_")
+
+    # Log metrics for the test set
+    sklr_test_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_test, y_test, prefix="test_")
+
+    # Display the logged metrics
+    sklr_val_metrics = {k.replace("val_", ""): v for k, v in sklr_val_metrics.items()}
+    sklr_test_metrics = {k.replace("test_", ""): v for k, v in sklr_test_metrics.items()}
+    display(pd.DataFrame([sklr_val_metrics, sklr_test_metrics], index=["validation", "test"]))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Patch pandas version in logged model
+# MAGIC
+# MAGIC Ensures that model serving uses the same version of pandas that was used to train the model.
+
+# COMMAND ----------
+
+import mlflow
+import os
+import shutil
+import tempfile
+import yaml
+
+run_id = mlflow_run.info.run_id
+
+# Set up a local dir for downloading the artifacts.
+tmp_dir = str(tempfile.TemporaryDirectory())
+os.makedirs(tmp_dir)
+
+client = mlflow.tracking.MlflowClient()
+
+# Fix conda.yaml
+conda_file_path = client.download_artifacts(run_id, "model/conda.yaml", dst_path=tmp_dir)
+with open(conda_file_path) as f:
+  conda_libs = yaml.load(f, Loader=yaml.FullLoader)
+pandas_lib_exists = any([lib.startswith("pandas==") for lib in conda_libs["dependencies"][-1]["pip"]])
+if not pandas_lib_exists:
+  print("Adding pandas dependency to conda.yaml")
+  conda_libs["dependencies"][-1]["pip"].append(f"pandas=={pd.__version__}")
+
+  with open(f"{tmp_dir}/conda.yaml", "w") as f:
+    f.write(yaml.dump(conda_libs))
+  client.log_artifact(run_id=run_id, local_path=conda_file_path, artifact_path="model")
+
+# Fix requirements.txt
+venv_file_path = client.download_artifacts(run_id, "model/requirements.txt", dst_path=tmp_dir)
+with open(venv_file_path) as f:
+  venv_libs = f.readlines()
+venv_libs = [lib.strip() for lib in venv_libs]
+pandas_lib_exists = any([lib.startswith("pandas==") for lib in venv_libs])
+if not pandas_lib_exists:
+  print("Adding pandas dependency to requirements.txt")
+  venv_libs.append(f"pandas=={pd.__version__}")
+
+  with open(f"{tmp_dir}/requirements.txt", "w") as f:
+    f.write("\n".join(venv_libs))
+  client.log_artifact(run_id=run_id, local_path=venv_file_path, artifact_path="model")
+
+shutil.rmtree(tmp_dir)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Feature importance
+# MAGIC
+# MAGIC SHAP is a game-theoretic approach to explain machine learning models, providing a summary plot
+# MAGIC of the relationship between features and model output. Features are ranked in descending order of
+# MAGIC importance, and impact/color describe the correlation between the feature and the target variable.
+# MAGIC - Generating SHAP feature importance is a very memory intensive operation, so to ensure that AutoML can run trials without
+# MAGIC   running out of memory, we disable SHAP by default.<br />
+# MAGIC   You can set the flag defined below to `shap_enabled = True` and re-run this notebook to see the SHAP plots.
+# MAGIC - To reduce the computational overhead of each trial, a single example is sampled from the validation set to explain.<br />
+# MAGIC   For more thorough results, increase the sample size of explanations, or provide your own examples to explain.
+# MAGIC - SHAP cannot explain models using data with nulls; if your dataset has any, both the background data and
+# MAGIC   examples to explain will be imputed using the mode (most frequent values). This affects the computed
+# MAGIC   SHAP values, as the imputed samples may not match the actual data distribution.
+# MAGIC
+# MAGIC For more information on how to read Shapley values, see the [SHAP documentation](https://shap.readthedocs.io/en/latest/example_notebooks/overviews/An%20introduction%20to%20explainable%20AI%20with%20Shapley%20values.html).
+
+# COMMAND ----------
+
+# Set this flag to True and re-run the notebook to see the SHAP plots
+shap_enabled = True
+
+# COMMAND ----------
+
+if shap_enabled:
+    mlflow.autolog(disable=True)
+    mlflow.sklearn.autolog(disable=True)
+    from shap import KernelExplainer, summary_plot
+    # Sample background data for SHAP Explainer. Increase the sample size to reduce variance.
+    train_sample = X_train.sample(n=min(100, len(X_train.index)))
+
+    # Sample a single example from the validation set to explain. Increase the sample size and rerun for more thorough results.
+    example = X_val.sample(n=1)
+
+    # Use Kernel SHAP to explain feature importance on the example from the validation set.
+    predict = lambda x: model.predict_proba(pd.DataFrame(x, columns=X_train.columns))
+    explainer = KernelExplainer(predict, train_sample, link="logit")
+    shap_values = explainer.shap_values(example, l1_reg=False, nsamples=500)
+    summary_plot(shap_values, example, class_names=model.classes_)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Inference
+# MAGIC [The MLflow Model Registry](https://docs.databricks.com/applications/mlflow/model-registry.html) is a collaborative hub where teams can share ML models, work together from experimentation to online testing and production, integrate with approval and governance workflows, and monitor ML deployments and their performance. The snippets below show how to add the model trained in this notebook to the model registry and to retrieve it later for inference.
+# MAGIC
+# MAGIC > **NOTE:** The `model_uri` for the model already trained in this notebook can be found in the cell below
+# MAGIC
+# MAGIC ### Register to Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC registered_model_version = mlflow.register_model(model_uri, model_name)
+# MAGIC ```
+# MAGIC
+# MAGIC ### Load from Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC model_version = registered_model_version.version
+# MAGIC
+# MAGIC model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
+# MAGIC model.predict(input_X)
+# MAGIC ```
+# MAGIC
+# MAGIC ### Load model without registering
+# MAGIC ```
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC
+# MAGIC model = mlflow.pyfunc.load_model(model_uri)
+# MAGIC model.predict(input_X)
+# MAGIC ```
+
+# COMMAND ----------
+
+# model_uri for the generated model
+print(f"runs:/{ mlflow_run.info.run_id }/model")
